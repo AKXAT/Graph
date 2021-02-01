964213814548e0d8b2c96626b04109fd73dfbd10
unknown <akshat.aksharma@gmail.com>
2021-02-01 15:39:05 +0530

    Added get path feature

--- Graph.py
+++ Graph.py
@@ -8,7 +8,20 @@ class Graph:
             else:
                 self.graph_dict[start]=[end]
         print(self.graph_dict)
-
+    def get_paths(self,start,end,path=[]):
+        self.path = path
+        path = path + [start]
+        if start == end :
+            return path
+        if start not in self.graph_dict:
+            return []
+        paths=[]
+        for node in self.graph_dict[start]:
+            if node not in path :
+                new_path = self.get_paths(node,end,path)
+                for p in new_path:
+                    paths.append(p)
+        return paths
 
 if __name__ == '__main__':
     routes = [
@@ -20,3 +33,6 @@ if __name__ == '__main__':
         ("New York","Toronto")
     ]
     route_graph = Graph(routes)
+    start = 'Mumbai'
+    end = 'New York'
+    print(route_graph.get_paths(start,end))
